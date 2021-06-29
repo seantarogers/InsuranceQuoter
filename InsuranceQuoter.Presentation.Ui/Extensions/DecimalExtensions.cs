092324fe24126a1bc139545bfcf24ba909e831de
Sean Rogers <a.seanthomasrogers@gmail.com>
2021-06-29 15:22:39 +0100

    convert to pounds

---
+++ InsuranceQuoter.Presentation.Ui/Extensions/DecimalExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace InsuranceQuoter.Presentation.Ui.Extensions
+{
+    public static class DecimalExtensions
+    {
+        public static string ToPounds(this decimal amount) =>
+            amount.ToString("C").Replace("GBP", "£");
+    }
+}
